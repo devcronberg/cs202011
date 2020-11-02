672c9872c4cca2cc01bc2c0f0d8ed7650d4be665
Admin <Admin@TA-826-INS.kursdom.net>
2020-11-02 08:25:00 +0100

    Add project files.

---
+++ Start/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

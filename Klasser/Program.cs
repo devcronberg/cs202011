ffc96bc7160cf23a28758912b2a80df70befea4f
Michell <Admin@TA-826-INS>
2020-11-03 16:00:43 +0100

    slut dag 2

---
+++ Klasser/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Terning t = new Terning();
+            //t.ErSekser = Sekser;
+            //t.Værdi = 1;
+            //t.Skriv();
+            //t.Ryst();
+            //t.Skriv();
+            //t.Ryst();
+            //t.Skriv();
+
+            LudoTerning t = new LudoTerning();
+            t.Ryst();
+            t.Skriv();
+            Console.WriteLine(t.ErStjerne());
+
+            //Terning t2 = new Terning();
+            //t2.Ryst();
+            //t2.Skriv();
+
+        }
+
+        public static void Sekser()
+        {
+            Console.WriteLine("!!!!");
+            Console.Beep();
+
+        }
+    }
+
+
+
+
+
+
+
+    public class Terning {
+
+        public Action ErSekser;
+        private int værdi;
+
+        public int Værdi
+        {
+            get {
+                // log
+                // sikkerhed
+                return værdi;
+
+            }
+            set {
+                // log
+                // validering
+                if (value < 1 || value > 6)
+                    value = 1;
+                værdi = value;
+
+            }
+        }
+
+        public void Ryst() {
+            this.Værdi = new Random().Next(1, 7);
+            if (this.værdi == 6)
+                ErSekser.Invoke();
+
+        }
+
+        public void Skriv()
+        {
+            Console.WriteLine("[ " + this.værdi + " ]");
+        }
+
+        // felter     = Værdier
+        // egenskaber = Indkapsling af felter
+        // metoder    = Funktionalitet
+        // hændelser  = Afkobling
+
+
+
+    }
+
+    public class LudoTerning : Terning{
+        public bool ErStjerne() {
+            return this.Værdi == 3;
+        }
+    }
+}

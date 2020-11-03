edb4622ce546976f22537304309fcb1533eba082
Michell <Admin@TA-826-INS>
2020-11-03 15:15:24 +0100

    test

---
+++ Samlinger/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+//using System.Collections;
+using System.Collections.Generic;
+
+namespace Samlinger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList lst = new ArrayList();
+            //lst.Add("Mikkel");
+            //lst.Add("Mathias");
+
+            //lst.Add("Lene");
+            //lst.Insert(1, "Michell");
+
+            //foreach (var item in lst)
+            //    Console.WriteLine(item);
+            //lst.RemoveAt(1);
+
+            //foreach (var item in lst)
+            //    Console.WriteLine(item);
+
+            //lst.Add(1);
+
+            //foreach (var item in lst)
+            //    Console.WriteLine(item);
+
+            //string navn = lst[0] as string;
+            //Console.WriteLine(navn);
+
+            List<string> lst = new List<string>();
+            lst.Add("Mikkel");
+            lst.Add("Mathias");
+            lst.Add("Lene");
+            foreach (var item in lst)
+                Console.WriteLine(item);
+
+            string navn = lst[0];
+            Console.WriteLine(navn);
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            int t = q.Dequeue();
+
+            Stack<string> s = new Stack<string>();
+            s.Push("S8");
+            s.Push("R2");
+            s.Push("RK");
+            string kort = s.Pop();
+
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            d.Add("a", 100);
+            d.Add("b", 200);
+            int v = d["b"];
+            
+        }
+    }
+}

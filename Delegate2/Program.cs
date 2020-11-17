f7e82a1a99eaddb332e585849f311387b144a3f5
Michell Cronberg <Admin@TA-822-INS>
2020-11-17 15:39:32 +0100

    .

---
+++ Delegate2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Delegate2
+{
+
+    public delegate void DelegateVoid();
+    public delegate void DelegateStringVoid(string a);
+    public delegate bool DelegateStringIntBool(string a, int b);
+    public delegate int DelegateIntIntInt(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Direkte kald
+            Test1();
+
+            // Inddirekte 
+            DelegateVoid d1 = new DelegateVoid(Test1);
+            d1.Invoke();
+            Kør(d1);
+
+            DelegateVoid d2 = HentDelegate();
+            d2.Invoke();
+
+
+            //DelegateIntIntInt d3 = new DelegateIntIntInt(LægSammen);
+            DelegateIntIntInt d3 = LægSammen;
+            // Direkte
+            Console.WriteLine(LægSammen(1,1));
+            // Inddirekte
+            //Console.WriteLine(d3.Invoke(1, 1));
+            Console.WriteLine(d3(1, 1));
+
+
+            DelegateIntIntInt minRegner = FindTilfældigRegneArt();
+            Console.WriteLine(minRegner.Invoke(5,5));
+
+
+
+        }
+
+        static void Kør(DelegateVoid func) {
+            // kljdfhgklsdjgf
+            // æjdhafgkldjhfgdkls
+            func.Invoke();
+        }
+
+        static DelegateVoid HentDelegate() { 
+            return new DelegateVoid(Test1); 
+        }
+
+        static DelegateIntIntInt FindTilfældigRegneArt() {
+            if (DateTime.Now.Millisecond % 2 == 0) {
+                return new DelegateIntIntInt(LægSammen);
+            } else {
+                return new DelegateIntIntInt(TrækFra);
+            }
+        
+        }
+        static void Test1()
+        {
+            Console.WriteLine("I Test1");
+        }
+
+        static int LægSammen(int a, int b) {
+
+            return a + b;
+
+        }
+
+        static int TrækFra(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
---
+++ DelegateDemo1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegateDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> lst = new List<int> {5,1,7,1,8,3 ,9};
+            //var res = lst.Where(Min);
+            var res = lst.Where(i => i < 5);
+
+
+            Maskine m = new Maskine();
+            //m.LogDelegate += F;
+            //m.LogDelegate += C;
+            //m.LogDelegate += R;
+            //m.LogDelegate += (string txt) => { Console.WriteLine(txt); };
+            m.LogDelegate += txt => Console.WriteLine(txt);
+            m.Start();
+            m.Slut();
+        }
+
+        static bool Min(int tal) {
+            if (tal < 5)
+                return true;
+            else
+                return false;
+        }
+
+        static void C(string t) {
+            Console.WriteLine(t);
+        }
+
+        static void R(string t, int a)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(t);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        static void F(string t) {
+             System.IO.File.AppendAllText(@"c:\temp\log.txt", t);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    class Maskine
+    {
+
+        public Action<string> LogDelegate;
+
+        public Maskine()
+        {
+
+        }
+
+        public void Start()
+        {
+            Log("Starter");
+        }
+
+        public void Slut()
+        {
+            Log("Slut");
+        }
+
+        private void Log(string txt)
+        {
+            if (LogDelegate != null)
+            {
+                LogDelegate.Invoke(DateTime.Now.ToShortTimeString() + " " + txt);
+            }
+            
+
+            
+        }
+    }
+}
